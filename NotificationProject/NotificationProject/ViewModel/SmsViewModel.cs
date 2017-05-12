d74817bc886b0334e73b1ce9ee0a321e0bf0b4d1
bob <o.baptiste@wanadoo.fr>
2017-05-12 13:48:01 +0200

    Rajout d'une vue sms

---
+++ NotificationProject/NotificationProject/ViewModel/SmsViewModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NotificationProject.HelperClasses;
+using System.Windows.Input;
+
+namespace NotificationProject.ViewModel
+{
+    class SmsViewModel : ObservableObject, IPageViewModel
+    {
+
+        #region constructor
+        public SmsViewModel()
+        {
+            ButtonCommand = new RelayCommand(o => SendMessage(), n => CanSend());
+        }
+        #endregion
+
+        #region Fields
+        private string _phoneNumber;
+        private string _smsText;
+        public ICommand ButtonCommand { get; set; }
+        #endregion
+
+
+        #region Properties
+        public string Name
+        {
+            get
+            {
+                return "Sms View";
+            }
+        }
+
+        public string PhoneNumber
+        {
+            get
+            {
+                return _phoneNumber;
+            }
+            set
+            {
+                _phoneNumber = value;
+            }
+        }
+
+        public string SmsText
+        {
+            get
+            {
+                return _smsText;
+            }
+            set
+            {
+                _smsText = value;
+            }
+        }
+        #endregion
+
+        #region Command
+        private void SendMessage()
+        {
+
+        }
+
+        private bool CanSend()
+        {
+            return !String.IsNullOrEmpty(SmsText) && !String.IsNullOrEmpty(PhoneNumber);
+        }
+        #endregion
+
+
+    }
+}
