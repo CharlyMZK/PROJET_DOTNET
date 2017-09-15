c10d311fa40f19ecadc2bb8126223ce79e6bdc4c
CharlyMzk <charlymzk.contact@gmail.com>
2017-09-15 15:53:48 +0200

    Notification configuration model + sample

---
+++ NotificationProject/DataAccess/Model/NotificationConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Model
+{
+    class NotificationConfiguration
+    {
+        private Boolean isEnabled { get; set; }
+        private Boolean smsEnabled { get; set; }
+        private Boolean callEnabled { get; set; }
+        private Boolean otherEnabled { get; set; }
+
+        public NotificationConfiguration(bool isEnabled, bool smsEnabled, bool callEnabled, bool otherEnabled)
+        {
+            this.isEnabled = isEnabled;
+            this.smsEnabled = smsEnabled;
+            this.callEnabled = callEnabled;
+            this.otherEnabled = otherEnabled;
+        }
+    }
+}
