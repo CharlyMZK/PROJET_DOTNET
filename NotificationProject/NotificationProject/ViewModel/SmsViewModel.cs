8e0354a3fb1ce60422b9f48b99aeb99a8589a0d3
Laurent <lfreyss4@gmail.com>
2017-09-29 13:34:35 +0200

    Beginning new theme

--- NotificationProject/NotificationProject/ViewModel/SmsViewModel.cs
+++ NotificationProject/NotificationProject/ViewModel/SmsViewModel.cs
@@ -8,6 +8,8 @@ using System.Windows.Input;
 using DataAccess.Model;
 using System.Collections.ObjectModel;
 using NotificationProjet.Controller;
+using System.Xml;
+using System.IO;
 
 namespace NotificationProject.ViewModel
 {
@@ -94,11 +96,14 @@ namespace NotificationProject.ViewModel
         {
             try
             {
+                WriteConversationOnXml();
                 SelectedDevice.sendMessage(JSONHandler.creationSMSString("bob", SelectedDevice.Name, SmsText, PhoneNumber));
             }
             catch (Exception ex)
             {
                 //TODO popup message fail
+                Console.WriteLine(ex);
+
             }
         }
 
@@ -116,12 +121,59 @@ namespace NotificationProject.ViewModel
            catch(Exception ex)
             {
                 //TODO faire popup fail message
+                Console.WriteLine(ex);
             }
         }
 
         private bool CanCall()
         {
             return !String.IsNullOrEmpty(PhoneNumber) && SelectedDevice != null;
+        }
+
+        private void WriteConversationOnXml() {
+
+            if (File.Exists(PhoneNumber + ".xml"))
+            {
+                string filename = PhoneNumber + ".xml";
+                XmlDocument doc = new XmlDocument();
+                //load from file
+                doc.Load(filename);
+
+                //create node and add value
+                XmlNode node = doc.CreateNode(XmlNodeType.Element, "Envoi", null);
+                //create title node
+                XmlNode nodeDate = doc.CreateElement("DateTime");
+                //add value for it
+                nodeDate.InnerText = DateTime.Today.ToString();
+
+                //create Url node
+                XmlNode nodeMessage = doc.CreateElement("Message");
+                nodeMessage.InnerText = SmsText;
+
+                //add to parent node
+                node.AppendChild(nodeDate);
+                node.AppendChild(nodeMessage);
+
+                //add to elements collection
+                doc.DocumentElement.AppendChild(node);
+
+                //save back
+                doc.Save(filename);
+
+            }
+            else
+            {
+                using (XmlWriter writer = XmlWriter.Create(PhoneNumber + ".xml"))
+                {
+                    writer.WriteStartDocument();
+                    writer.WriteStartElement("Envoi");
+                    writer.WriteElementString("DateTime", DateTime.Today.ToString());
+                    writer.WriteElementString("Message", SmsText);
+                    writer.WriteEndElement();
+                    writer.WriteEndDocument();
+                }
+            }
+           
         }
         #endregion
 
